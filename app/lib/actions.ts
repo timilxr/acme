8ce5111fbae115dd7ea4935a47c91dd4eb7e851f
timilxr <38140090+timilxr@users.noreply.github.com>
2024-08-11 16:22:38 +0100

    added credentials authentication using NextAuth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { sql } from '@vercel/postgres';
 import {z} from 'zod';
 import {revalidatePath} from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
   errors?: {
@@ -112,5 +114,24 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
         return {
             message: 'Database Error: Failed to Delete Invoice.'
         }
+    }
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
     }
   }
